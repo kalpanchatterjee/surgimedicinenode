b8f022008e5d6a715e5777769044da3b93cb011d
Kalpan Chattopadhyay <kalpanchattopdahyay@stridus.com>
2022-02-22 18:16:09 +0530

    commited

--- routes/loginReg.js
+++ routes/loginReg.js
@@ -11,6 +11,8 @@ router.post("/register", (req, res) => {
   let email = req.body.email;
   let phone = req.body.phone;
   let password = req.body.password;
+  let role_id = req.body.role_id;
+  role_id = role_id == null ? 2 : role_id;
 
   let searchsql =
     "select count(*) as count from co_users where email='" + email + "'";
@@ -18,10 +20,10 @@ router.post("/register", (req, res) => {
     if (countres[0].count == 0) {
       bcrypt.hash(password, saltRounds, (err, hash) => {
         var query =
-          "INSERT INTO co_users (first_name, last_name,email,phone,password) VALUES (?,?,?,?,?)";
+          "INSERT INTO co_users (first_name, last_name,email,phone,password,role_id) VALUES (?,?,?,?,?,?)";
         db.query(
           query,
-          [first_name, last_name, email, phone, hash],
+          [first_name, last_name, email, phone, hash, role_id],
           function (err, result) {
             if (err) throw err;
             else res.send("user registered succesfully");
@@ -38,7 +40,7 @@ router.post("/login", (req, res) => {
   let username = req.body.email;
   let password = req.body.password;
 
-  let loginquery = "select * from co_users where email='" + username + "'";
+  let loginquery = "select * from co_users  where email='" + username + "'";
   db.query(loginquery, function (err, user) {
     if (user.length > 0) {
       bcrypt.compare(password, user[0].password, function (err, passmatch) {
--- routes/product.js
+++ routes/product.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../config");
-
 const app = express();
+const dotenv = require("dotenv");
+const fs = require("fs");
+dotenv.config();
+
+//muler config
+const multer = require("multer");
+// Create multer object
+const imageUpload = multer({
+  dest: "images",
+});
+
+//Aws related keys and imports
+const AWS = require("aws-sdk");
+const { reset } = require("nodemon");
+const ID = process.env.s3accessid;
+const SECRET = process.env.s3accesskey;
+const BUCKET_NAME = process.env.s3bucketname;
+
+const s3 = new AWS.S3({
+  accessKeyId: ID,
+  secretAccessKey: SECRET,
+});
 //cors origin
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -22,7 +43,10 @@ router.post("/addProduct", (req, res) => {
   let product_desc = req.body.product_desc;
   let price = req.body.price;
   let category_id = req.body.category_id;
+  let product_image_id = req.body.product_image_id;
+  product_image_id = product_image_id.split(",");
 
+  let newproduct_id = "";
   let insertquery =
     "INSERT INTO co_products (product_name, product_desc,price,category_id) VALUES (?,?,?,?)";
 
@@ -30,8 +54,21 @@ router.post("/addProduct", (req, res) => {
     insertquery,
     [product_name, product_desc, price, category_id],
     (err, result) => {
+      newproduct_id = result.insertId;
       if (err) throw err;
-      else res.send("product inserted successfully");
+
+      for (let i = 0; i < product_image_id.length - 1; i++) {
+        let newQuery =
+          "update co_product_images set product_id=" +
+          newproduct_id +
+          " where product_image_id=" +
+          product_image_id[i];
+        // console.log(newQuery);
+        db.query(newQuery, (err, result) => {
+          if (err) throw err;
+        });
+      }
+      res.json("product inserted successfully");
     }
   );
 });
@@ -40,7 +77,7 @@ router.get("/getProducts", (req, res) => {
   //let query = "select * from co_products  order by category_id asc";
 
   let query =
-    "select  cp.product_id,cp.product_name,cp.product_desc,cp.price,cp.category_id,cc.category_name  from co_products  cp left join co_categories cc on cp.category_id=cc.category_id order by category_id asc ";
+    "select  cp.product_id,cp.product_name,cp.product_desc,cp.price,cp.category_id,cc.category_name,(select product_image_name from co_product_images where product_id=cp.product_id limit 1)as image_name  from co_products  cp left join co_categories cc on cp.category_id=cc.category_id left join co_product_images cpi on cp.product_id=cpi.product_id order by category_id asc ";
 
   db.query(query, (err, result) => {
     if (err) throw err;
@@ -77,21 +114,99 @@ router.put("/updateProduct", (req, res) => {
 
 router.delete("/deleteProduct/:id", (req, res) => {
   let deleteQuery = "delete from co_products where product_id=" + req.params.id;
-  console.log(deleteQuery);
+  // console.log(deleteQuery);
   db.query(deleteQuery, (err, result) => {
     res.send("Product successfully deleted");
   });
 });
 
-router.get("/getProductsByCategory/:id", (req, res) => {
+router.post("/getProductsByCategory/:id", (req, res) => {
   let category_id = req.params.id;
   let limit = req.body.limit;
   let index = req.body.index;
-  let query = "select * from co_products where category_id=" + category_id;
+  let query =
+    "select (select count(*) from co_products where category_id=" +
+    category_id +
+    ")as total_product,cp.product_id,cp.product_name,cp.product_desc,cp.price,cp.category_id,cc.category_name,(select product_image_name from co_product_images where product_id=cp.product_id limit 1)as image_name  from co_products  cp left join co_categories cc on cp.category_id=cc.category_id  where cp.category_id=" +
+    category_id;
 
-  if (index != null && index != "" && limit != null && limit != "") {
+  if (index != null && limit != "") {
     query += " LIMIT " + index + "," + limit;
   }
+
+  db.query(query, (err, result) => {
+    if (err) throw err;
+    else res.json(result);
+  });
+});
+
+router.post("/uploadImage", imageUpload.single("image"), (req, res) => {
+  const fileContent = fs.readFileSync(req.file.path);
+
+  // Setting up S3 upload parameters
+  const params = {
+    Bucket: BUCKET_NAME,
+    Key: req.file.filename, // File name you want to save as in S3
+    Body: fileContent,
+  };
+
+  // Uploading files to the bucket imageUpload.single("image"),
+  try {
+    s3.upload(params, function (err, data) {
+      if (err) {
+        // throw err;
+      }
+      console.log(`File uploaded successfully. ${data.Location}`);
+    });
+  } catch (error) {
+    // console.log(error);
+  }
+
+  fs.unlinkSync(req.file.path);
+
+  let product_id = req.body.product_id;
+  product_id = product_id == null ? 0 : product_id;
+
+  // console.log("product-f", product_id, req.params.product_id);
+  // console.log(product_id);
+  let new_image_id = "";
+  let insertQuery =
+    "insert into co_product_images (product_id,product_image_name,status,created_date,created_timestamp)VALUES (" +
+    product_id +
+    ",'" +
+    req.file.filename +
+    "','Y',current_date,CURRENT_TIMESTAMP)";
+
+  try {
+    db.query(insertQuery, (err, data) => {
+      // console.log(data.insertId);
+      new_image_id = data.insertId;
+      if (err) console.log(err);
+      else res.send(new_image_id.toString());
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  //res.send("success");
+});
+router.get("/getProductImages/:key", (req, res) => {
+  let key = req.params.key;
+  let downloadParam = {
+    Key: key,
+    Bucket: BUCKET_NAME,
+  };
+  try {
+    const readStream = s3.getObject(downloadParam).createReadStream();
+    readStream.pipe(res);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/getImagesofProduct/:id", (req, res) => {
+  let product_id = req.params.id;
+  let query = "select * from co_product_images where product_id=" + product_id;
   db.query(query, (err, result) => {
     if (err) throw err;
     else res.json(result);
